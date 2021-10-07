87efff2f1d1149f3a1cca1e2e5a6e5480ef71acf
rors <959655+rors@users.noreply.github.com>
2021-10-07 07:53:04 -0400

    Initial commit of project files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # project-2
+
 The starting point for a browser extension project
---
+++ content.js
@@ -0,0 +1,6 @@
+var elements = document.getElementsByTagName('p');
+
+for (var i = 0; i < elements.length; i++) {
+   var element = elements[i];
+}
+
